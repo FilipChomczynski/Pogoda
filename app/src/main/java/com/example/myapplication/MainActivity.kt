08fbee2275dd3bb15517289027b20d50bc04abfa
Filip <f.chomczynski@gmail.com>
2024-04-23 15:31:51 +0200

    wyszukiwanie miast

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -12,6 +14,8 @@ import okhttp3.Response
 import org.json.JSONObject
 import java.io.IOException
 
+
+
 class MainActivity : AppCompatActivity() {
 
     private val client = OkHttpClient()
@@ -19,7 +23,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=Bielsko-Biala&lang=pl&appid=29604613abf856e1b8f45210c7ae7562"
+        getWeather("Bielsko-Biała")
+
+        val search = findViewById<EditText>(R.id.search).text.toString()
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            getWeather(search)
+        }
+    }
+
+    fun getWeather(miasto: String) {
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?q=$miasto&lang=pl&appid=29604613abf856e1b8f45210c7ae7562"
         var data: JSONObject = JSONObject("{}")
 
         val request = Request.Builder()
@@ -33,15 +47,14 @@ class MainActivity : AppCompatActivity() {
                 data = JSONObject(response.body()?.string())
 
                 runOnUiThread {
-                    findViewById<TextView>(R.id.temperatura).text = data.getJSONObject("main").getString("temp")
-                    findViewById<TextView>(R.id.stan).text = data.getString("name")
+                    findViewById<TextView>(R.id.miasto).text = data.getString("name")
+                    findViewById<TextView>(R.id.stan).text = data.getJSONArray("weather").getJSONObject(0).getString("description")
 
 
                     findViewById<TextView>(R.id.temperatura).text = "temperatura: " + data.getJSONObject("main").getString("temp")
                     findViewById<TextView>(R.id.wiatr).text = "wiatr: " + data.getJSONObject("wind").getString("speed")
-                    findViewById<TextView>(R.id.cisnienie).text = "cisnienie: " + data.getJSONObject("wind").getString("speed")
                     findViewById<TextView>(R.id.wilgotnosc).text = "wilgotnosc: " + data.getJSONObject("main").getString("humidity")
-                    findViewById<TextView>(R.id.widocznosc).text = "widocznosc: " + data.getString("visibility")
+
                 }
             }
         })
